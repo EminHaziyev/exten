26b8862c0ac12998e2cc945f9df46dbd4d501320
EminHaziyev <mn05x.mail@gmail.com>
2023-09-02 12:16:31 +0400

    first commit

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,7 @@
 const iframe = document.createElement("iframe");
 const el = document.getElementsByClassName("ytd-page-manager")[0];
 // Set attributes for the iframe
-iframe.src = "https://final.syntheyesis.repl.co"; // Replace with the URL you want to embed
+iframe.src = "https://final.syntheyesis.repl.co/detect"; // Replace with the URL you want to embed
 iframe.width = "400";
 iframe.height = "1000";
 iframe.id = "finall";
@@ -15,4 +15,4 @@ iframe. allow="camera https://final.syntheyesis.repl.co"
 
 
 document.body.appendChild(iframe);
-// iframe.contentWindow.document.getElementsByTagName("H1")[0];
+console.log(iframe.contentWindow.document.getElementsByClassName("blend-shapes-label"));
